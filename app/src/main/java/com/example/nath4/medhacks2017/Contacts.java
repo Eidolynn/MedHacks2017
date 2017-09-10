6ee84bf48d76194a95f97a46521441544d2af154
Shane Park <seyeon9897@gmail.com>
2017-09-10 02:05:39 -0400

    ContactSend

---
+++ app/src/main/java/com/example/nath4/medhacks2017/Contacts.java
@@ -0,0 +1,21 @@
+package com.example.nath4.medhacks2017;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Contacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        
+    }
+}
