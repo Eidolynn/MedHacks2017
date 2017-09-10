fb12955f57e553a79226537dc1e0fd799b2219a2
Shane Park <seyeon9897@gmail.com>
2017-09-10 09:46:33 -0400

    Update WatchVid.java

--- app/src/main/java/com/example/nath4/medhacks2017/WatchVid.java
+++ app/src/main/java/com/example/nath4/medhacks2017/WatchVid.java
@@ -1,7 +1,12 @@
 package com.example.nath4.medhacks2017;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
+import android.net.Uri; //Required to use URIs
 
 public class WatchVid extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class WatchVid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_vid);
+
+        VideoView video = (VideoView)findViewById(R.id.videoView);
+        String path = "android.resource://com.example.nath4.medhacks2017/"+R.raw.pill;
+        Uri uri = Uri.parse(path);
+        video.setVideoURI(uri);
+        video.requestFocus();
+        video.start();
     }
+
 }
